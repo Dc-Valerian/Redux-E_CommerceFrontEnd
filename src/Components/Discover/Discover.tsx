4bca80b8b8366564e03e361efd074b985035ac6d
Dc-Valerian <valerianpedro03@gmail.com>
2023-02-26 14:36:12 +0100

    saved update

--- src/Components/Discover/Discover.tsx
+++ src/Components/Discover/Discover.tsx
@@ -1,26 +1,30 @@
 import React from 'react'
 import styled from 'styled-components'
+import img1 from "../Assets/1.png"
+import img2 from "../Assets/2.png"
+import img3 from "../Assets/3.png"
 
 const Discover = () => {
   return (
     <div>
         <Container>
             <Head>
-
+                Discover More.
+                <span>Good Things are waiting for you</span>
             </Head>
             <CardHolder>
                 <Card>
                     <First>
                         <Title>
-
+                            Explore New Arrivals
                         </Title>
                         <Sub>
-
+                            Shop From the Lastest Top Brands
                         </Sub>
                         <Button>
-
+                            Show All
                         </Button>
-                        <Second/>
+                        <Second src={img1}/>
                     </First>
                 </Card>
             </CardHolder>
@@ -45,6 +49,21 @@ const Card = styled.div``
 
 const CardHolder = styled.div``
 
-const Head = styled.div``
+const Head = styled.div`
+font-size: 30px;
+margin-bottom: 10px;
+font-weight: 600;
 
-const Container = styled.div``
+span{
+    color: gray;
+}
+`
+
+const Container = styled.div`
+display: flex;
+justify-content: center;
+flex-direction: column;
+padding-left: 50px;
+overflow: hidden;
+margin-top: 80px;
+`
