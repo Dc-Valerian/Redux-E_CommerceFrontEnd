840053fac69e2cbf58aa897f0078969a9f675e2b
Dc-Valerian <valerianpedro03@gmail.com>
2023-02-26 22:27:17 +0100

    saved changes

--- src/Components/Product/Product.tsx
+++ src/Components/Product/Product.tsx
@@ -1,5 +1,12 @@
 import React from 'react'
 import styled from 'styled-components'
+import img1 from "../Assets/dark.jpg"
+import img2 from "../Assets/bulb.jpg"
+import img3 from "../Assets/code.jpg"
+import img4 from "../Assets/fine.jpg"
+import img5 from "../Assets/great.jpg"
+import img6 from "../Assets/hmm.jpg"
+import {AiFillStar} from "react-icons/ai"
 
 const Product = () => {
   return (
@@ -8,27 +15,28 @@ const Product = () => {
             <Head>
                 New Arrivals.{" "}
                 <span>
-                  Val BackPacks & Bags
+                  Valerian BackPacks & Bags
                 </span>
             </Head>
             <CardHolder>
               <Card>
                 <MainImage>
                   <ImageHold>
-                    <Img/>
+                    <Img src={img1}/>
                   </ImageHold>
                   <Title>
-
+                    Dark Wings
                   </Title>
                   <Description>
-
+                    New Designs 2023
                   </Description>
                   <PriceHold>
                     <Price>
-
+                      $90.00
                     </Price>
                     <Rating>
-
+                   <span>   <AiFillStar/></span>
+                   5.5(87 Reviews)
                     </Rating>
                   </PriceHold>
                 </MainImage>
@@ -40,9 +48,16 @@ const Product = () => {
 }
 
 export default Product
-const ImageHold = styled.div``
+const ImageHold = styled.div`
+height: 250px;
+width: 250px;
+border-radius: 5px;
+`
 
-const Img = styled.img``
+const Img = styled.img`
+width: 200px;
+object-fit: contain;
+`
 
 const Title = styled.div``
 
@@ -56,10 +71,33 @@ const PriceHold = styled.div``
 
 const MainImage = styled.div``
 
-const Card = styled.div``
+const Card = styled.div`
+margin: 10px;
+`
 
-const CardHolder = styled.div``
+const CardHolder = styled.div`
+display: flex;
+flex-wrap: wrap;
+align-items: center;
+padding-bottom: 70px;
+`
 
-const Head = styled.div``
+const Head = styled.div`
+font-size: 30px;
+margin-bottom: 10px;
+font-weight: 600;
 
-const Container = styled.div``
+span{
+  color: grey;
+}
+`
+
+const Container = styled.div`
+display: flex;
+justify-content: center;
+flex-direction: column;
+padding-left: 50px;
+overflow: hidden;
+margin-bottom: 50px;
+border-bottom: 2px dotted grey;
+`
