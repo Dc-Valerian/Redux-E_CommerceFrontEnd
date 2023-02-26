6e8e2975c56288b37e9f198d9ce2cb872adf1740
Dc-Valerian <valerianpedro03@gmail.com>
2023-02-26 13:33:03 +0100

    started the discover page

---
+++ src/Components/Discover/Discover.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Discover = () => {
+  return (
+    <div>
+        <Container>
+            <Head>
+
+            </Head>
+            <CardHolder>
+                <Card>
+                    <First>
+                        <Title>
+
+                        </Title>
+                        <Sub>
+
+                        </Sub>
+                        <Button>
+
+                        </Button>
+                        <Second/>
+                    </First>
+                </Card>
+            </CardHolder>
+        </Container>
+    </div>
+  )
+}
+
+export default Discover
+
+const Button = styled.button``
+
+const Second = styled.img``
+
+const Sub = styled.div``
+
+const Title = styled.div``
+
+const First = styled.div``
+
+const Card = styled.div``
+
+const CardHolder = styled.div``
+
+const Head = styled.div``
+
+const Container = styled.div``
