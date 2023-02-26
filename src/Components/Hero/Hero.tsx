1ba4277632ee65af0a2f8a52dfa8ce6a132a11cc
Dc-Valerian <valerianpedro03@gmail.com>
2023-02-26 13:17:53 +0100

    started the hero page

--- src/Components/Hero/Hero.tsx
+++ src/Components/Hero/Hero.tsx
@@ -1,40 +1,57 @@
 import React from 'react'
 import Slider from "react-slick";
 import styled from 'styled-components';
+import hero from "../Assets/hero1.png"
 // import 
 
 const Hero = () => {
-    const settings = {
-        dots: true,
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        speed: 2000,
-        autoplaySpeed: 2000,
-        cssEase: "linear"
-      };
+    // const settings = {
+    //     dots: true,
+    //     infinite: true,
+    //     slidesToShow: 3,
+    //     slidesToScroll: 1,
+    //     autoplay: true,
+    //     speed: 2000,
+    //     autoplaySpeed: 2000,
+    //     cssEase: "linear"
+    //   };
   return (
     <div>
        <Container>
        <First>
-
+        <Title>In this season,Find the best✨🕊️</Title>
+        <Heading>
+          Exclusive Collection
+          <br /> for everyone
+        </Heading>
+        <Button>
+          Explore now
+        </Button>
        </First>
-       <Second>
-        <Img>
-        
-        </Img>
-       </Second>
+      <Second src={hero}/>
        </Container>
      </div>
   )
 }
 
 export default Hero
+const Button = styled.button``
+
+const Title = styled.div``
+
+const Heading = styled.div``
+
 const Img = styled.div``
 
-const Second = styled.div``
+const Second = styled.img``
 
 const First = styled.div``
 
-const Container = styled.div``
+const Container = styled.div`
+width: 100%;
+background-color:#e3ffe6;
+display: flex;
+justify-content: center;
+align-items: center;
+padding-bottom: 40px;
+`
