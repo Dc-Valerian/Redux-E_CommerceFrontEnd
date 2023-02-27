b649fc2f8caeb70cd51bffecf406c45fac7b5751
Dc-Valerian <valerianpedro03@gmail.com>
2023-02-27 02:13:03 +0100

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import AllRoutes from './Components/AllRoutes/AllRoutes';
+import Footer from './Components/Footer/Footer';
 import Header from './Components/Header/Header';
 
 
@@ -9,6 +10,7 @@ function App() {
     <div>
       <Header/>
       <AllRoutes/>
+      <Footer/>
     </div>
   );
 }
